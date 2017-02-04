297dd2017e4a53ee579d1fab0c9285cc2408a0ad
aristotelesbr2014 <aristotelesbr2014@gmail.com>
2017-02-04 10:46:14 -0200

    add validateions to Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,13 +4,18 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
   def create
     @article = Article.new(article_params)
-    @article.save
-    flash[:success] = "Article has been created"
-    redirect_to articles_path
+    # @article.user = current_user
+    if @article.save
+      flash[:success] = "Article has been created"
+      redirect_to articles_path
+    else
+      flash.now[:danger] = "Article has not been created"
+      render :new
+    end
   end
-
   private
 
   def article_params
